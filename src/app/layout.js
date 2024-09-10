f5024f8401d032d28873d4c7fd16688ea3f61f56
josephine19001 <gyameraj@gmail.com>
2024-09-10 14:27:25 +0300

    Wrapped children around movie context

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import MoviesProvider from '@/context/MoviesContext';
 
 export const metadata = {
   title: 'Movies App',
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MoviesProvider>{children}</MoviesProvider>
+      </body>
     </html>
   );
 }
