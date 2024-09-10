59736f95ed3b573f6ccc31008d428f033053cb03
josephine19001 <gyameraj@gmail.com>
2024-09-10 14:04:49 +0300

    Added pagination

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -6,16 +6,25 @@ const usePopularMovies = () => {
   const [popularMovies, setPopularMovies] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPage] = useState(0);
 
-  const getPopularMovies = useCallback(async () => {
+  const getPopularMovies = useCallback(async (page = 1) => {
     try {
       setLoading(true);
       setError(false);
+
       const response = await axios.get(`${MOVIES_BASE_API_URL}/popular`, {
-        params: moviesParams
+        params: {
+          ...moviesParams,
+          page
+        }
       });
       const movies = response.data.results;
+
       setPopularMovies(movies);
+      setCurrentPage(page);
+      setTotalPage(response.data.total_pages);
     } catch (error) {
       setError(true);
     } finally {
@@ -23,11 +32,23 @@ const usePopularMovies = () => {
     }
   }, []);
 
+  const goToNextPage = () => {
+    if (currentPage < totalPages) getPopularMovies(currentPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) getPopularMovies(currentPage - 1);
+  };
+
   return {
     getPopularMovies,
     error,
     loading,
-    popularMovies
+    popularMovies,
+    currentPage,
+    totalPages,
+    goToNextPage,
+    goToPreviousPage
   };
 };
 
